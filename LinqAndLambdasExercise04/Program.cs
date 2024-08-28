aa73ed30cefd06fb20079e865daa3997a4dd28bd
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-28 19:13:12 +0200

    Add: data

--- LinqAndLambdasExercise04/Program.cs
+++ LinqAndLambdasExercise04/Program.cs
@@ -10,10 +10,19 @@ namespace LinqAndLambdasExercise04
     {
         static void Main(string[] args)
         {
-            TallGuy tallGuy = new TallGuy() {Name = "Jimmy", Height = 76 };
-            tallGuy.TalkAboutYourself();
-            Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
-            tallGuy.Honk();
+            //TallGuy tallGuy = new TallGuy() {Name = "Jimmy", Height = 76 };
+            //tallGuy.TalkAboutYourself();
+            //Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
+            //tallGuy.Honk();
+
+            int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+            IEnumerable<int> result =
+                from v in values
+                where v < 37
+                orderby -v
+                select v;
+
+            var res = values.Where(v => v < 37).OrderBy(v => -v);
         }
     }
 }
